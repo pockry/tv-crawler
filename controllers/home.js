fcc3f506cda2199c20ed5c9cd3a3b0f3884dd32f
pockry <pockry@outlook.com>
2014-11-28 01:12:29 +0800

    add module superagent

--- controllers/home.js
+++ controllers/home.js
@@ -1,10 +1,17 @@
+var superagent = require('superagent');
+var async = require('async');
+
 /**
  * GET /
  * Home page.
  */
 
-exports.index = function(req, res) {
-  res.render('home', {
-    title: '主页'
-  });
+exports.index = function(req, res, next) {
+	superagent.get('http://thetvdb.com/api/GetSeries.php?seriesname=')
+		.end(function(err, result) {
+			if(err) {return next(err);}
+			//console.log(result.body);
+			
+		});
+  
 };

9c42d713ff0cda877060cd7c1b079c410a21a215
pockry <pockry@outlook.com>
2014-11-28 19:57:25 +0800

    little change

--- controllers/home.js
+++ controllers/home.js
@@ -7,11 +7,21 @@ var async = require('async');
  */
 
 exports.index = function(req, res, next) {
-	superagent.get('http://thetvdb.com/api/GetSeries.php?seriesname=')
+	var apikey = '1b6d64de3bfe79d1b8ce7ac3bacee468';
+	var rawurl = 'http://api.trakt.tv/calendar/premieres.json/' + apikey;
+	var date = '20110101';
+	var days = 1;
+	var url = rawurl + '/' + date + '/' + days;
+	console.log(url);
+	superagent.get(url)
 		.end(function(err, result) {
 			if(err) {return next(err);}
 			//console.log(result.body);
 			
+			res.render('home', {
+				title: 'index',
+				result: result.body[0]
+			});
 		});
   
 };
